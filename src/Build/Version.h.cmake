f02dab9362d84a4ad7df17ea7f9e555d825d1ce2
heshanshan-hanhua <heshanshan@hhintech.com>
2026-01-13 08:47:52 +0800

    Add files via upload

---
+++ src/Build/Version.h.cmake
@@ -0,0 +1,12 @@
+
+// Version Number
+#define FCVersionMajor "${PACKAGE_VERSION_MAJOR}"
+#define FCVersionMinor "${PACKAGE_VERSION_MINOR}"
+#define FCVersionName  "${PACKAGE_VERSION_NAME}"
+#define FCVersionPatch "${PACKAGE_VERSION_PATCH}"
+// test: $Format:Hash (%H), Date: %ci$
+#define FCRevision      "${PACKAGE_WCREF}"      //Highest committed revision number
+#define FCRevisionDate  "${PACKAGE_WCDATE}"     //Date of highest committed revision
+#define FCRepositoryURL ""      // Repository URL of the working copy PACKAGE_WCURL
+#define FunctionConfig  "${PACKAGE_FUNCTION_CONFIG}"
+
